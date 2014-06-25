89e1c496307312184c2a476e2dd2cd9eee6ad3bb
Timothee Habra <timothee.habra@uclouvain.be>
2014-06-25 17:07:39 +0200

    add empty RobotranMotionControlBoard from Alberto

---
+++ Standalone/src/generic/robotran_yarp_plugin/include/RobotranMotionControlBoard.h
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2007-2013 Istituto Italiano di Tecnologia ADVR & iCub Facility
+ * Authors: Enrico Mingo, Alessio Rocchi, Mirko Ferrati, Silvio Traversaro and Alessandro Settimi
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#ifndef ROBOTRAN_YARP_CONTROLBOARD_H
+#define ROBOTRAN_YARP_CONTROLBOARD_H
+
+#include <yarp/sig/all.h>
+#include <yarp/os/all.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Property.h>
+#include <yarp/dev/Drivers.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+#include <yarp/dev/IControlMode.h>
+#include <yarp/sig/Vector.h>
+#include <yarp/os/Time.h>
+
+
+const double ROBOT_POSITION_TOLERANCE = 0.9;
+
+namespace yarp {
+    namespace dev {
+        class RobotranYarpMotionControl;
+    }
+    
+}
+
+
+class yarp::dev::RobotranYarpMotionControl:
+    public DeviceDriver,
+    public IPositionControl2,
+    public IVelocityControl,
+    public IEncodersTimed,
+//    public IControlMode,
+    public IPositionDirect
+{
+public:
+    
+    RobotranYarpMotionControl();
+
+    virtual ~RobotranYarpMotionControl();
+
+    /**
+     * robotran stuff
+     */
+    bool robotran_init();
+    void onUpdate(/* whatever*/);
+
+    /**
+     * Yarp interfaces start here
+     */
+    
+    //DEVICE DRIVER
+    virtual bool open(yarp::os::Searchable& config);    
+    virtual bool close();
+    
+    //ENCODERS
+    virtual bool getEncoder(int j, double *v); //WORKS
+    virtual bool getEncoders(double *encs); //WORKS    
+    virtual bool resetEncoder(int j); //WORKS
+    virtual bool resetEncoders(); //WORKS
+    virtual bool setEncoder(int j, double val); //WORKS
+    virtual bool setEncoders(const double *vals); //WORKS 
+    
+    virtual bool getEncoderSpeed(int j, double *sp); //NOT TESTED
+    virtual bool getEncoderSpeeds(double *spds); //NOT TESTED 
+    
+    virtual bool getEncoderAcceleration(int j, double *spds); //NOT IMPLEMENTED
+    virtual bool getEncoderAccelerations(double *accs); //NOT IMPLEMENTED
+
+    // ENCODERS TIMED
+    virtual bool getEncodersTimed(double *encs, double *time);
+    virtual bool getEncoderTimed(int j, double *encs, double *time);
+
+    //POSITION CONTROL
+    virtual bool stop(int j); //WORKS
+    virtual bool stop(); //WORKS
+    virtual bool positionMove(int j, double ref); //WORKS
+    virtual bool getAxes(int *ax); // WORKS
+    virtual bool positionMove(const double *refs); //WORKS
+    /// @arg sp [deg/sec]
+    virtual bool setRefSpeed(int j, double sp); //WORKS
+    virtual bool getRefSpeed(int j, double *ref); //WORKS
+    virtual bool getRefSpeeds(double *spds); //WORKS
+    
+    virtual bool relativeMove(int j, double delta); //NOT TESTED
+    virtual bool relativeMove(const double *deltas); //NOT TESTED
+    virtual bool checkMotionDone(int j, bool *flag); //NOT TESTED
+    virtual bool checkMotionDone(bool *flag); //NOT TESTED
+    virtual bool setPositionMode(); //NOT TESTED
+
+    // POS 2
+    virtual bool positionMove(const int n_joint, const int *joints, const double *refs);
+    virtual bool relativeMove(const int n_joint, const int *joints, const double *deltas);
+    virtual bool checkMotionDone(const int n_joint, const int *joints, bool *flags);
+    virtual bool setRefSpeeds(const int n_joint, const int *joints, const double *spds);
+    virtual bool setRefAccelerations(const int n_joint, const int *joints, const double *accs);
+    virtual bool getRefSpeeds(const int n_joint, const int *joints, double *spds);
+    virtual bool getRefAccelerations(const int n_joint, const int *joints, double *accs);
+    virtual bool stop(const int n_joint, const int *joints);
+
+    /// @arg spds [deg/sec]
+    virtual bool setRefSpeeds(const double *spds); //NOT TESTED
+    
+    virtual bool setRefAcceleration(int j, double acc); //NOT IMPLEMENTED
+    virtual bool setRefAccelerations(const double *accs); //NOT IMPLEMENTED
+    virtual bool getRefAcceleration(int j, double *acc); //NOT IMPLEMENTED
+    virtual bool getRefAccelerations(double *accs); //NOT IMPLEMENTED
+    
+    //VELOCITY CONTROL
+    virtual bool setVelocityMode(); //NOT TESTED
+    virtual bool velocityMove(int j, double sp); //NOT TESTED    
+    virtual bool velocityMove(const double *sp); //NOT TESTED    
+    
+    /*CONTROL MODE
+    virtual bool setPositionMode(int j); //WORKS    
+    virtual bool setVelocityMode(int j); //WORKS
+    virtual bool getControlMode(int j, int *mode); //WORKS
+    
+    virtual bool setTorqueMode(int j); //NOT TESTED 
+    virtual bool getControlModes(int *modes); //NOT TESTED
+    
+    virtual bool setImpedancePositionMode(int j);//NOT IMPLEMENTED
+    virtual bool setImpedanceVelocityMode(int j); //NOT IMPLEMENTED
+    virtual bool setOpenLoopMode(int j); //NOT IMPLEMENTED
+    */
+
+    /*IMPEDANCE CTRL
+    virtual bool getImpedance(int j, double *stiffness, double *damping); // [Nm/deg] & [Nm*sec/deg]
+    virtual bool setImpedance(int j, double stiffness, double damping); // [Nm/deg] & [Nm*sec/deg]
+    virtual bool setImpedanceOffset(int j, double offset);
+    virtual bool getImpedanceOffset(int j, double* offset);
+    virtual bool getCurrentImpedanceLimit(int j, double *min_stiff, double *max_stiff, double *min_damp, double *max_damp);
+    */
+
+    /*
+     * IPidControl Interface methods
+     *
+    virtual bool setPid (int j, const Pid &pid);
+    virtual bool setPids (const Pid *pids);
+    virtual bool setReference (int j, double ref);
+    virtual bool setReferences (const double *refs);
+    virtual bool setErrorLimit (int j, double limit);
+    virtual bool setErrorLimits (const double *limits);
+    virtual bool getError (int j, double *err);
+    virtual bool getErrors (double *errs);
+    virtual bool getPid (int j, Pid *pid);
+    virtual bool getPids (Pid *pids);
+    virtual bool getReference (int j, double *ref);
+    virtual bool getReferences (double *refs);
+    virtual bool getErrorLimit (int j, double *limit);
+    virtual bool getErrorLimits (double *limits);
+    virtual bool resetPid (int j);
+    virtual bool disablePid (int j);
+    virtual bool enablePid (int j);
+    virtual bool setOffset (int j, double v);
+    */
+
+    // IPOSITION DIRECT
+    bool setPositionDirectMode();
+    bool setPosition(int j, double ref);
+    bool setPositions(const int n_joint, const int *joints, double *refs);
+    bool setPositions(const double *refs);
+    
+private:
+	
+    /* PID structures */
+    struct PID {
+        double p;
+        double i;
+        double d;
+        double maxInt;
+        double maxOut;
+    };
+    
+    unsigned int numberOfJoints;
+
+    //Contains the parameters of the device contained in the yarpConfigurationFile .ini file
+    yarp::os::Property pluginParameters;
+    
+     /**
+     * The GAZEBO desired position of each joints, (output of trajectory interp)
+     */
+    yarp::sig::Vector desiredPosition;
+    
+    /**
+     * The zero position is the position of the GAZEBO joint that will be read as the starting one
+     * i.e. getEncoder(j)=zeroPosition+gazebo.getEncoder(j);
+     */
+    yarp::sig::Vector zeroPosition;
+
+    yarp::sig::Vector vel, speed, acc, amp, torque;
+    yarp::os::Semaphore pos_lock;
+    yarp::sig::Vector referenceSpeed, referencePosition, referenceAcceleraton, referenceTorque;
+    yarp::sig::Vector max_pos, min_pos;
+
+
+
+
+
+
+};
+
+#endif //ROBOTRAN_YARP_CONTROLBOARD_H
---
+++ Standalone/src/generic/robotran_yarp_plugin/src/RobotranMotionControlBoard.cpp
@@ -0,0 +1,350 @@
+/*
+ * Copyright (C) 2007-2013 Istituto Italiano di Tecnologia ADVR & iCub Facility
+ * Authors: Enrico Mingo, Alessio Rocchi, Mirko Ferrati, Silvio Traversaro and Alessandro Settimi
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#include <RobotranMotionControlBoard.h>
+
+
+using namespace yarp::dev;
+
+
+/**
+ * robotran stuff
+ */
+bool RobotranYarpMotionControl::robotran_init()
+{
+    std::cout << "robotran_init " << std::endl;
+    return true;
+}
+
+void RobotranYarpMotionControl::onUpdate(/* whatever*/)
+{
+
+}
+
+/////////////////////////////////////
+// DEVICE DRIVER
+/////////////////////////////////////
+
+RobotranYarpMotionControl::RobotranYarpMotionControl()
+{
+
+}
+
+RobotranYarpMotionControl::~RobotranYarpMotionControl()
+{
+
+}
+
+bool RobotranYarpMotionControl::open(yarp::os::Searchable& config)
+{
+    std::cout << "robotran motionControl parameters are " << config.toString() << std::endl;
+    config.check("joint");
+
+    config.find("joint").isInt();
+
+    int joint = config.find("joint").asInt();
+
+    std::cout << "joint is " << joint;
+    return true;
+}
+
+bool RobotranYarpMotionControl::close()
+{
+    std::cout << "closing robotran motionControl " << std::endl;
+    return true;
+}
+
+
+/////////////////////////////////////
+// POSITION CONTROL
+/////////////////////////////////////
+
+bool RobotranYarpMotionControl::positionMove(int j, double ref) //WORKS
+{
+    std::cout << "robotran motionControl: positionMove " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::stop(int j) //WORKS
+{
+    std::cout << "robotran motionControl: stop " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::stop() //WORKS
+{
+    std::cout << "robotran motionControl: stop " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::positionMove(const double *refs) //WORKS
+{
+    std::cout << "robotran motionControl: positionMove " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getAxes(int *ax) // WORKS
+{
+    std::cout << "robotran motionControl: getAxes " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setRefSpeed(int j, double sp) //WORKS
+{
+    std::cout << "robotran motionControl: setRefSpeed " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getRefSpeed(int j, double *ref) //WORKS
+{
+    std::cout << "robotran motionControl: getRefSpeed " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getRefSpeeds(double *spds) //WORKS
+{
+    std::cout << "robotran motionControl: getRefSpeeds " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::relativeMove(int j, double delta) //NOT TESTED
+{
+    std::cout << "robotran motionControl: relativeMove " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::relativeMove(const double *deltas) //NOT TESTED
+{
+    std::cout << "robotran motionControl: relativeMove " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::checkMotionDone(int j, bool *flag) //NOT TESTED
+{
+    std::cout << "robotran motionControl: checkMotionDone " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::checkMotionDone(bool *flag) //NOT TESTED
+{
+    std::cout << "robotran motionControl: checkMotionDone " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setPositionMode() //NOT TESTED
+{
+    std::cout << "robotran motionControl: setPositionMode " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setRefSpeeds(const double *spds) //NOT TESTED
+{
+    std::cout << "robotran motionControl: setRefSpeeds " << std::endl;
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::setRefAcceleration(int j, double acc) //NOT IMPLEMENTED
+{
+    std::cout << "robotran motionControl: setRefAcceleration " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setRefAccelerations(const double *accs) //NOT IMPLEMENTED
+{
+    std::cout << "robotran motionControl: setRefAccelerations " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getRefAcceleration(int j, double *acc) //NOT IMPLEMENTED
+{
+    std::cout << "robotran motionControl: getRefAcceleration " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getRefAccelerations(double *accs) //NOT IMPLEMENTED
+{
+    std::cout << "robotran motionControl: getRefAccelerations " << std::endl;
+    return false;
+}
+
+// IPositionControl2
+
+bool RobotranYarpMotionControl::positionMove(const int n_joint, const int *joints, const double *refs) //NOT IMPLEMENTED
+{
+    std::cout << "robotran motionControl: positionMove " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::relativeMove(const int n_joint, const int *joints, const double *deltas) //NOT IMPLEMENTED
+{
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::checkMotionDone(const int n_joint, const int *joints, bool *flags) //NOT IMPLEMENTED
+{
+    return false;
+}
+
+bool RobotranYarpMotionControl::setRefSpeeds(const int n_joint, const int *joints, const double *spds) //NOT IMPLEMENTED
+{
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::setRefAccelerations(const int n_joint, const int *joints, const double *accs) //NOT IMPLEMENTED
+{
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::getRefSpeeds(const int n_joint, const int *joints, double *spds) //NOT IMPLEMENTED
+{
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::getRefAccelerations(const int n_joint, const int *joints, double *accs) //NOT IMPLEMENTED
+{
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::stop(const int n_joint, const int *joints) //NOT IMPLEMENTED
+{
+    return false;
+}
+
+/////////////////////////////////////
+// POSITION DIRECT
+/////////////////////////////////////
+
+bool RobotranYarpMotionControl::setPositionDirectMode() //NOT IMPLEMENTED -> Is it the same as setPositionMode?
+{
+    return false;
+}
+
+bool RobotranYarpMotionControl::setPosition(int j, double ref)
+{
+    std::cout << "robotran motionControl: setPosition " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setPositions(const int n_joint, const int *joints, double *refs)
+{
+    std::cout << "robotran motionControl: setPositions " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setPositions(const double *refs)
+{
+    std::cout << "robotran motionControl: setPositions " << std::endl;
+    return false;
+}
+
+/////////////////////////////////////
+// VELOCITY CONTROL
+/////////////////////////////////////
+
+
+bool RobotranYarpMotionControl::setVelocityMode() //NOT TESTED
+{
+    std::cout << "robotran motionControl: setVelocityMode " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::velocityMove(int j, double sp) //NOT TESTED
+{
+    std::cout << "robotran motionControl: velocityMove " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::velocityMove(const double *sp) //NOT TESTED
+{
+    std::cout << "robotran motionControl: velocityMove " << std::endl;
+    return false;
+}
+
+
+/////////////////////////////////////
+// ENCODER
+/////////////////////////////////////
+
+bool RobotranYarpMotionControl::getEncoder(int j, double *v) //WORKS
+{
+    std::cout << "robotran motionControl: getEncoder " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getEncoders(double *encs) //WORKS
+{
+    std::cout << "robotran motionControl: getEncoders " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getEncodersTimed(double *encs, double *time)
+{
+    std::cout << "robotran motionControl: getEncodersTimed " << std::endl;
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::getEncoderTimed(int j, double *encs, double *time)
+{
+    std::cout << "robotran motionControl: getEncoderTimed " << std::endl;
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::resetEncoder(int j) //WORKS
+{
+    std::cout << "robotran motionControl: resetEncoder " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::resetEncoders() //WORKS
+{
+    std::cout << "robotran motionControl: resetEncoders " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setEncoder(int j, double val) //WORKS
+{
+    std::cout << "robotran motionControl: setEncoder " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::setEncoders(const double *vals) //WORKS
+{
+    std::cout << "robotran motionControl: setEncoders " << std::endl;
+    return false;
+}
+
+
+bool RobotranYarpMotionControl::getEncoderSpeed(int j, double *sp) //NOT TESTED
+{
+    std::cout << "robotran motionControl: getEncoderSpeed " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getEncoderSpeeds(double *spds) //NOT TESTED
+{
+    std::cout << "robotran motionControl: getEncoderSpeeds " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getEncoderAcceleration(int j, double *spds) //NOT IMPLEMENTED
+{
+    std::cout << "robotran motionControl: getEncoderAcceleration " << std::endl;
+    return false;
+}
+
+bool RobotranYarpMotionControl::getEncoderAccelerations(double *accs) //NOT IMPLEMENTED
+{
+    std::cout << "robotran motionControl: getEncoderAccelerations " << std::endl;
+    return false;
+}
