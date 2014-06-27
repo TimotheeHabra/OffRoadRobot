082da0bc9c08fabbdbbf802c59980cc87cda1f4a
Timothee Habra <timothee.habra@uclouvain.be>
2014-06-27 15:40:40 +0200

    delete control board in yarp_finish

--- Standalone/src/project/yarp_files/yarp_finish.cpp
+++ Standalone/src/project/yarp_files/yarp_finish.cpp
@@ -5,13 +5,18 @@
 
 using namespace std;
 
-void yarp_finish()
+void yarp_finish(void* RobotranYarp_interface)
 {
 	// here should come termination of yarp
 	// - closing port
 	// - removing Robotran-Yarp drivers
 
 	cout << "termination of yarp" << endl;
+
+	yarp::dev::PolyDriver *p_controlBoard;
+	p_controlBoard = (yarp::dev::PolyDriver*)RobotranYarp_interface;  // convert back into object
+
+	delete(p_controlBoard);
 
 }
 
